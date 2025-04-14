a230b80f94b6a06818c2f2602ef18e6938242995
NorthWest999 <storkflorian@yahoo.de>
2025-04-14 12:33:45 +0200

    ChainlinkPriceConsumer.sol

--- contracts/ChainlinkPriceConsumer.sol
+++ contracts/ChainlinkPriceConsumer.sol
@@ -3,22 +3,33 @@ pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+/// @title Chainlink Price Consumer
+/// @notice This contract fetches the latest price data from a Chainlink price feed.
 contract ChainlinkPriceConsumer {
     AggregatorV3Interface internal priceFeed;
 
-    // z.B. ETH/USD auf Ethereum Mainnet
+    /// @notice Constructor initializes the price feed address.
+    /// @param _priceFeed The address of the Chainlink price feed.
     constructor(address _priceFeed) {
+        require(_priceFeed != address(0), "Invalid price feed address");
         priceFeed = AggregatorV3Interface(_priceFeed);
     }
 
-    function getLatestPrice() public view returns (int) {
+    /// @notice Fetches the latest price from the Chainlink price feed.
+    /// @return price The latest price data.
+    /// @return updatedAt The timestamp of the last price update.
+    function getLatestPrice() public view returns (int price, uint updatedAt) {
         (
-            , 
-            int price,
             ,
+            price,
             ,
+            updatedAt,
             
         ) = priceFeed.latestRoundData();
-        return price;
+
+        require(price > 0, "Invalid price data");
+        require(block.timestamp - updatedAt < 1 days, "Price data is outdated");
+
+        return (price, updatedAt);
     }
 }
